ddd2bf6a5871edb1b99ceee851ee480daeb07f7c
Billy Mueller <william.mueller@nih.gov>
2021-09-20 11:31:26 -0400

    ran_can k is now variable

--- R/ran_can.R
+++ R/ran_can.R
@@ -12,33 +12,34 @@
 #' @param random_vect a character vector where each element represents an optional one-sided formula for the random-effects in the linear mixed model. Covariates
 #'   with a random-effect are separated by +. By default, an intercept is included. If no intercept, -1 should be the first term included.
 #' @param subject name of the covariate representing the grouping structure specified with ''.
+#' @param k number of classes
 #' @param par boolean argument specifying if models should be run in parallel
 #' @return a list that has lcmem output corresponding with the random_vect provided.
 #' @export
 
 
-ran_can <- function(df, fixed, mixture, random_vect, subject, par = F){
+ran_can <- function(df, fixed, mixture, random_vect, subject, k = 5, par = F){
   df <- substitute(...(df = df))$df
 
   if(par ==F){
-    ran_can_base(df, fixed, mixture, random_vect, subject)
+    ran_can_base(df, fixed, mixture, random_vect, subject, k)
   } else{
-    ran_can_par(df, fixed, mixture, random_vect, subject)
+    ran_can_par(df, fixed, mixture, random_vect, subject, k)
   }
 }
 
-ran_can_base <- function(df, fixed, mixture, random_vect, subject){
+ran_can_base <- function(df, fixed, mixture, random_vect, subject, k){
   mos <- lapply(1: length(random_vect), function(i){
-    mo <- lcmem(df = df, fixed = fixed, mixture = mixture, random = random_vect[[i]], subject = subject, k = 5)
+    mo <- lcmem(df = df, fixed = fixed, mixture = mixture, random = random_vect[[i]], subject = subject, k = k)
   })
   return(mos)
 }
 
 
-ran_can_par <- function(df, fixed, mixture, random_vect, subject){
+ran_can_par <- function(df, fixed, mixture, random_vect, subject, k){
   mos <- listenv()
   for(i in length(forms)){
-    mos[[i]] %<% lcmem(df = df, fixed = fixed, mixture = mixture, random = random_vect[[i]], subject = subject, k = 5)
+    mos[[i]] %<% lcmem(df = df, fixed = fixed, mixture = mixture, random = random_vect[[i]], subject = subject, k = k)
   }
   mos <- as.list(mos)
   return(mos)
