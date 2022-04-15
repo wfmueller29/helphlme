612fd8da851b469ec1459a6e51b4247485132819
Billy Mueller <wfmueller29@gmail.com>
2022-04-15 10:12:05 -0400

    +(rep_pat)

---
+++ R/rep_pat.R
@@ -0,0 +1,10 @@
+#' Replace Pattern
+#'
+#' Function that literally replaces patterns in specified columns using
+#' another columns as the value for replacement
+#'
+#' @param data a dataframe which contains the patterns and replacements
+#' @param cols character vector specifying columns in data to look for the
+#' pattern
+#' @param rep_cols
+#'
