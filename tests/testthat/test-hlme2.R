6af28989043f516f29b9fb5500fffb40cc003cd5
Billy Mueller <muellerwf@nih.gov>
2022-04-15 17:09:47 -0400

    +(tests) -(coecer_call, cross_call)

---
+++ tests/testthat/test-hlme2.R
@@ -0,0 +1,124 @@
+# Lets see if hlme2 provides us with the same results
+library(lcmm)
+library(helphlme)
+library(callframe)
+
+test_that("hlme2 works outside loop for ng = 1", {
+  our_model <- helphlme::hlme2(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    mixture = ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 1,
+    idiag = FALSE,
+    nwg = FALSE,
+    subject = "ID"
+  )
+  # we do not include nwg = FALSE argument in their model because
+  # this argument is removed by hlme2 when n = 1
+  their_model <- lcmm::hlme(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 1,
+    idiag = FALSE,
+    subject = "ID"
+  )
+  nearly_equal <- all.equal(our_model, their_model)
+  testthat::expect_true(nearly_equal)
+})
+
+test_that("hlme2 works for ng > 1", {
+  set.seed(100)
+  our_model <- helphlme::hlme2(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    mixture = ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 2,
+    idiag = FALSE,
+    nwg = FALSE,
+    subject = "ID"
+  )
+  # we do not include nwg = FALSE argument in their model because
+  # this argument is removed by hlme2 when n = 1
+  set.seed(100)
+  betas <- lcmm::hlme(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 1,
+    idiag = FALSE,
+    subject = "ID"
+
+  )
+  their_model <- lcmm::hlme(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    mixture = ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 2,
+    idiag = FALSE,
+    nwg = FALSE,
+    subject = "ID",
+    B = betas
+  )
+  nearly_equal <- all.equal(our_model, their_model)
+  testthat::expect_true(nearly_equal)
+})
+
+test_that("hlme2 works within for loop", {
+  set.seed(100)
+  our_models <- list()
+  for (i in seq_len(2)) {
+    i <- i
+    cat(i, "\n")
+    our_models[[i]] <- helphlme::hlme2(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    mixture = ~ X1 + X2 + X3,
+    random = ~1,
+    ng = i,
+    idiag = FALSE,
+    nwg = FALSE,
+    subject = "ID"
+  )
+    Sys.sleep(5)
+    cat(i)
+  }
+
+  set.seed(100)
+  their_models <- list()
+  their_models[[1]] <- lcmm::hlme(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 1L,
+    idiag = FALSE,
+    subject = "ID"
+  )
+
+  betas <- lcmm::hlme(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 1L,
+    idiag = FALSE,
+    subject = "ID"
+
+  )
+  their_models[[2]] <- lcmm::hlme(
+    data = data_hlme,
+    fixed = Y ~ X1 + X2 + X3,
+    mixture = ~ X1 + X2 + X3,
+    random = ~1,
+    ng = 2L,
+    idiag = FALSE,
+    nwg = FALSE,
+    subject = "ID",
+    B = betas
+  )
+
+  check <- all.equal(our_models, their_models)
+  testthat::expect_true(check)
+})
