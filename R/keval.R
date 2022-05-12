07e12dbbaa86759ed3c90b1cda986ee0968a18a8
Billy Mueller <muellerwf@nih.gov>
2022-05-12 11:55:24 -0400

    Refactor keval, also now compatible with hlme2

--- R/keval.R
+++ R/keval.R
@@ -8,29 +8,36 @@
 #' @param ... arguments to base plots (eg. main = "Title")
 #' @return kable output providing class analysis and plot output
 #' @export
-
+#' @example R\examples\keval.R
 
 keval <- function(df, model, age, ...) {
   sub <- paste0(
-    "k = ", model$parameters$k, "; Random = ", model$parameters$random,
-    "; idiag = ", model$parameters$idiag, "; nwg = ", model$parameters$nwg
+    "k = ",
+    paste(model$call$ng, collapse = ""),
+    "; Random = ",
+    paste(model$call$random, collapse = ""),
+    "; idiag = ",
+    paste(model$call$idiag, collapse = ""),
+    "; nwg = ",
+    paste(model$call$nwg, collapse = "")
   )
   lcplot(
     df = df,
-    model = model$model,
+    model = model,
     age = age,
     sub = sub,
     ...
   )
-  print(kableExtra::kable(ksum(model$model), caption = sub) %>%
-    kableExtra::kable_styling("striped", full_width = F))
+  print(kableExtra::kable(ksum(model), caption = sub) %>%
+          kableExtra::kable_styling("striped", full_width = F))
 }
 
 #' Model Class Evaluation for Each Model in List
 #'
 #' Provides n and frequency of each class and post prob of classes.
 #' Also plots the model results using lcplot function
-#' @param df data frame used to produce model. lcpred output
+#' @param df data frame used to produce model. Should be a dataframe outputted
+#' from the lcpred function.
 #' @param model_list List of lcmem output to evaluate
 #' @param age string of age variable
 #' @param ... arguments to base plots (eg. main = "Title")
@@ -39,24 +46,34 @@ keval <- function(df, model, age, ...) {
 #'   resutls = "asis"
 #' @details each plot and table will be accompanied by default by a string specifying k, random, idiag, and nwg
 #'   to identify each model
+#' @example R\examples\keval_apply.R
 #' @export
 
 
 keval_apply <- function(df, model_list, age, ...) {
   for (i in 1:length(model_list)) {
-    sub <- paste0(
-      "Model Number = ", i, "; k = ", model_list[[i]]$parameters$k, "; Random = ", model_list[[i]]$parameters$random,
-      "; idiag = ", model_list[[i]]$parameters$idiag, "; nwg = ", model_list[[i]]$parameters$nwg
-    )
-    lcplot(
-      df = df,
-      model = model_list[[i]]$model,
-      age = age,
-      sub = sub,
-      ...
-    )
-    print(kableExtra::kable(ksum(model_list[[i]]$model), caption = sub) %>%
-      kableExtra::kable_styling("striped", full_width = F))
+    model <- model_list[[i]]
+  sub <- paste0(
+    "Model Number = ",
+    i,
+    "; k = ",
+    paste(model$call$ng, collapse = ""),
+    "; Random = ",
+    paste(model$call$random, collapse = ""),
+    "; idiag = ",
+    paste(model$call$idiag, collapse = ""),
+    "; nwg = ",
+    paste(model$call$nwg, collapse = "")
+  )
+  lcplot(
+    df = df,
+    model = model,
+    age = age,
+    sub = sub,
+    ...
+  )
+  print(kableExtra::kable(ksum(model), caption = sub) %>%
+          kableExtra::kable_styling("striped", full_width = F))
   }
 }
 
