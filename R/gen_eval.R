2e28afba8be7fb3033aadfe99b3315cbc547efaa
Billy Mueller <muellerwf@nih.gov>
2022-05-12 11:53:52 -0400

    Refactor gen_eval, added Smallest Class
    Gen_eval now outputs more relevant model selection criteria
    and it also includes the smallest class proportion for the
    given model.

--- R/gen_eval.R
+++ R/gen_eval.R
@@ -1,18 +1,50 @@
 #' Evaluate models
 #'
 #' Outputs BIC and AIC for each model in the list provided
-#' @param list List of lcmem outputs from ran_can function
+#' @param model_list List of lcmem outputs from ran_can function
 #' @return data frame of with model number, model details, BIC and AIC other model selection criteria. This function
 #' also returns plots of the model selection criteria by model number.
 #' @export
+#' @example R\examples\mo_eval.R
 
-mo_eval <- function(list) {
-  tab <- as.data.frame(do.call(rbind, lapply(1:length(list), function(i) {
-    mo <- list[[i]]
-    a <- as.data.frame(summarytable(mo$model, mo$model, which = c("G", "loglik", "conv", "npm", "AIC", "BIC", "SABIC", "entropy", "ICL", "%class"), display = FALSE))
-    a <- a[1, ]
+mo_eval <- function(model_list) {
+  tab <- lapply(1:length(model_list), function(i) {
+    model <- model_list[[i]]
+    sum_table <- lcmm::summarytable(model,
+      model,
+      which = c(
+        "G",
+        "loglik",
+        "conv",
+        "npm",
+        "AIC",
+        "BIC",
+        "SABIC",
+        "entropy",
+        "ICL",
+        "%class"
+      ), display = FALSE
+    )
+
+    df_sum_table <- as.data.frame(sum_table)
+
+    per_class_names <- names(df_sum_table)[grepl(x = names(df_sum_table),
+                                                 pattern = "\\%class")]
+
+    smallest_class = min(df_sum_table[, per_class_names])
+
+
+    a <- df_sum_table[1, ]
     vect <- c(
-      "Model Number" = i, "Model" = paste0("Random = ", mo$parameters$random, "; idiag = ", mo$parameters$idiag, "; nwg = ", mo$parameters$nwg),
+      "Model Number" = i,
+      "Model" = paste0(
+        "Random = ",
+        paste(model$call$random, collapse = ""),
+        "; idiag = ",
+        paste(model$call$idiag, collapse = ""),
+        "; nwg = ",
+        paste(model$call$nwg, collapse = "")
+      ),
       "k" = a$G,
       "conv" = a$conv,
       "npm" = a$npm,
@@ -20,13 +52,27 @@ mo_eval <- function(list) {
       "BIC" = round(a$BIC),
       "AIC" = round(a$AIC),
       "entropy" = round(a$entropy, digits = 5),
-      "ICL" = round(a$ICL)
+      "ICL" = round(a$ICL),
+      "Smallest Class (%)" = smallest_class
     )
+
     return(vect)
-  })))
-  vars <- c("k", "conv", "npm", "loglik", "BIC", "AIC", "entropy", "ICL")
+  })
+
+  tab <- as.data.frame(do.call(rbind, tab))
+  vars <- c("k",
+            "conv",
+            "npm",
+            "loglik",
+            "BIC",
+            "AIC",
+            "entropy",
+            "ICL",
+            "Smallest Class (%)")
   tab[, vars] <- lapply(vars, function(var) as.numeric(tab[, var]))
-  plot <- pivot_longer(tab, npm:ICL, names_to = "mo_crit") %>%
+  plot <- tidyr::pivot_longer(tab,
+                              c(BIC, entropy, ICL, conv, k, `Smallest Class (%)`),
+                              names_to = "mo_crit") %>%
     ggplot(aes(x = factor(as.integer(`Model Number`)))) +
     geom_point(aes(y = value)) +
     geom_line(aes(y = value, group = "None")) +
